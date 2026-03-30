f75ed62fd63cad5c9e86dd028c1770c9f371f854
ramintahbaz23 <ramin@pattern23.ai>
2026-03-30 12:05:06 -0400

    Initial Commit

---
+++ spatialgalss/spatialgalssApp.swift
@@ -0,0 +1,17 @@
+//
+//  spatialgalssApp.swift
+//  spatialgalss
+//
+//  Created by Ramin on 3/30/26.
+//
+
+import SwiftUI
+
+@main
+struct spatialgalssApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
